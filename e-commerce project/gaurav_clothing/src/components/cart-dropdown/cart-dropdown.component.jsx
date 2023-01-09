d8618bb16d8fcf6760b6d0388901844a823eeac5
Gaurav <gt623794@gmail.com>
2023-01-09 17:24:41 +0530

    Checkout Page Almost Done

--- e-commerce project/gaurav_clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ e-commerce project/gaurav_clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../button/button.component";
@@ -7,13 +8,18 @@ import CartItem from "../cart-item/cart-item.component";
 
 const CartDropDown = () => {
     const {cartItems}=useContext(CartContext);
+    const navigate=useNavigate();
+
+    const goToCheckoutHandler=()=>{
+      navigate('/checkout')
+    }
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items" >
     {  cartItems.map(item =><CartItem key={item.id}  cartItem={item}/>)}
       </div>
 
-      <Button>CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
   );
 };
