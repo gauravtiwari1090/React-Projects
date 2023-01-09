d630e97ef616a069da4650104ed617926f54fca0
Gaurav <gt623794@gmail.com>
2023-01-09 21:10:49 +0530

    added total and  designed checkout page

--- e-commerce project/gaurav_clothing/src/contexts/cart.context.jsx
+++ e-commerce project/gaurav_clothing/src/contexts/cart.context.jsx
@@ -44,12 +44,14 @@ export const CartContext = createContext({
   removeItemFromCart: () => {},
   clearItemFromCart: () => {},
   cartCount: 0,
+  cartTotal:0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
   const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
 
   useEffect(() => {
     const newCartCount = cartItems.reduce(
@@ -58,6 +60,28 @@ export const CartProvider = ({ children }) => {
     );
     setCartCount(newCartCount);
   }, [cartItems]);
+
+
+//................
+
+useEffect(() => {
+  const newCartTotal = cartItems.reduce(
+    (total, cartItem) => total + cartItem.quantity*cartItem.price,
+    0
+  );
+  setCartTotal(newCartTotal);
+}, [cartItems]);
+
+
+
+
+
+
+
+
+
+
+
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd));
   };
@@ -79,6 +103,7 @@ export const CartProvider = ({ children }) => {
     clearItemFromCart,
     cartItems,
     cartCount,
+    cartTotal
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
